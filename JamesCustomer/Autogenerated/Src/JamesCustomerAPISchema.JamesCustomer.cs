bda3c1272f7b8f1ec0c21d280fb4300af365504d
Mirjalol Ziyadullayev <112009351+ziyadullayevmirjalol@users.noreply.github.com>
2024-12-14 17:59:56 +0500

    CustomerAPI

---
+++ JamesCustomer/Autogenerated/Src/JamesCustomerAPISchema.JamesCustomer.cs
@@ -0,0 +1,57 @@
+﻿namespace Terrasoft.Configuration
+{
+
+	using System;
+	using System.Collections.Generic;
+	using System.Collections.ObjectModel;
+	using System.Globalization;
+	using Terrasoft.Common;
+	using Terrasoft.Core;
+	using Terrasoft.Core.Configuration;
+
+	#region Class: JamesCustomerAPISchema
+
+	/// <exclude/>
+	public class JamesCustomerAPISchema : Terrasoft.Core.SourceCodeSchema
+	{
+
+		#region Constructors: Public
+
+		public JamesCustomerAPISchema(SourceCodeSchemaManager sourceCodeSchemaManager)
+			: base(sourceCodeSchemaManager) {
+		}
+
+		public JamesCustomerAPISchema(JamesCustomerAPISchema source)
+			: base( source) {
+		}
+
+		#endregion
+
+		#region Methods: Protected
+
+		protected override void InitializeProperties() {
+			base.InitializeProperties();
+			UId = new Guid("2207abdb-c309-4fb0-8ede-06a878a5d275");
+			Name = "JamesCustomerAPI";
+			ParentSchemaUId = new Guid("50e3acc0-26fc-4237-a095-849a1d534bd3");
+			CreatedInPackageId = new Guid("ffe40519-d915-43a8-a6f4-4e408b9194fa");
+			ZipBody = new byte[] { 31,139,8,0,0,0,0,0,4,0,83,0,0,69,207,108,233,1,0,0,0 };
+		}
+
+		#endregion
+
+		#region Methods: Public
+
+		public override void GetParentRealUIds(Collection<Guid> realUIds) {
+			base.GetParentRealUIds(realUIds);
+			realUIds.Add(new Guid("2207abdb-c309-4fb0-8ede-06a878a5d275"));
+		}
+
+		#endregion
+
+	}
+
+	#endregion
+
+}
+
---
+++ JamesCustomer/Schemas/JamesCustomerAPI/JamesCustomerAPI.cs
@@ -0,0 +1,71 @@
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.ServiceModel.Activation;
+using Terrasoft.Core.DB;
+using Terrasoft.Web.Common;
+using System;
+using System.Web.SessionState;
+using System.IO;
+using System.Globalization;
+using Terrasoft.Core;
+using Terrasoft.Common;
+
+namespace Terrasoft.Configuration
+{
+    [ServiceContract]
+    [AspNetCompatibilityRequirements(RequirementsMode = AspNetCompatibilityRequirementsMode.Required)]
+    public class CustomerAPI : BaseService, IReadOnlySessionState
+    {
+        [OperationContract]
+        [WebInvoke(Method = "GET", BodyStyle = WebMessageBodyStyle.Wrapped,
+            RequestFormat = WebMessageFormat.Json, ResponseFormat = WebMessageFormat.Json)]
+        public string GetAllCustomers()
+        {
+            try
+            {
+                var customers = new List<object>();
+
+                Select selectCustomers = new Select(UserConnection)
+                    .Column("Id")
+                    .Column("JamesFullName")
+                    .Column("JamesEmail")
+                    .Column("JamesPINFL")
+                    .From("JamesCustomer") as Select;
+
+                using (DBExecutor dbExecutor = UserConnection.EnsureDBConnection())
+                {
+                    using (IDataReader reader = selectCustomers.ExecuteReader(dbExecutor))
+                    {
+                        while (reader.Read())
+                        {
+                            customers.Add(new
+                            {
+                                Id = reader.GetColumnValue<Guid>("Id"),
+                                FullName = reader.GetColumnValue<string>("JamesFullName"),
+                                Email = reader.GetColumnValue<string>("JamesEmail"),
+                                PINFL = reader.GetColumnValue<string>("JamesPINFL")
+                            });
+                        }
+                    }
+                }
+
+                if (Customers.Count == 0)
+                {
+                    string errorMessage = "There are no customers yet!";
+                    var response = new HttpResponseMessage(HttpStatusCode.BadRequest);
+                    response.Headers.Add("X-Error-Message", errorMessage);
+                    return response;
+                }
+
+                return JsonConvert.SerializeObject(customers);
+            }
+            catch (Exception ex)
+            {
+                var response = new HttpResponseMessage(HttpStatusCode.InternalServerError);
+                string error = ex.Message;
+                response.Headers.Add("X-Error-Message", error);
+                return response;
+            }
+        }
+    }
+}
